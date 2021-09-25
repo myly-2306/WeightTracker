647ab3c5a53e9650bb5892e9b9d669c0230b717e
Derrick Nguyen <ptkn2001@msn.com>
2021-09-24 21:52:26 -0700

    fix seeding issue

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,25 +1,23 @@
+const Sequelize = require('sequelize');
 const sequelize = require('../config/connection');
 const { User, Plan } = require('../models');
-
 const userData = require('./userData.json');
 const planData = require('./planData.json');
 
 const createDatabase = async() => {
-    await sequelize.query(`DROP DATABASE IF EXISTS ${sequelize.config.database};`);
-    await sequelize.query(`CREATE DATABASE ${sequelize.config.database};`);
-    await sequelize.query(`USE ${sequelize.config.database};`)
+    const config = sequelize.config;
+    const sequelize2 = new Sequelize(`mysql://${config.username}:${config.password}@${config.host}:${config.port}`);
+    await sequelize2.query(`DROP DATABASE IF EXISTS ${config.database};`);
+    await sequelize2.query(`CREATE DATABASE ${config.database};`);
 }
 
 const seedDatabase = async() => {
     await createDatabase();
-
     await sequelize.sync({ force: true });
-
     await User.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
     });
-
     process.exit(0);
 };
 
